a99bd841172e26f2379ada185744dadfd88fd465
Ranjan Mishra <58240696+rrmishracts@users.noreply.github.com>
2019-11-27 12:55:40 -0500

    Update dao.py
    update the file 80%

--- PupperSearch/dao.py
+++ PupperSearch/dao.py
@@ -12,13 +12,13 @@ def readPupper(id):
     dbConnector.readPupper(read_sql, idList)
 
 def insertBreed(breed):
-        insert_sql = "insert into pythontest.breed (name,temperament,coat) values (%s,%s,%s)"
-        data = (breed.name,breed.temperament,breed.coat)
-        dbConnector.writeBreed(insert_sql, data)
+    insert_sql = "insert into pythontest.breed (name,temperament,coat) values (%s,%s,%s)"
+    data = (breed.name,breed.temperament,breed.coat)
+    dbConnector.writeBreed(insert_sql, data)
 
 
 def insertPupper(pupper):
-    insert_sql = "insert into pythontest.pupper (name,sex,weight,color,date_of_brith, breed_id) values (%s,%s,%s,%s,%s,%s)"
-    data = (pupper.name, pupper.sex, pupper.weight,pupper.color,pupper.date_of_brith, pupper.breed_id)
+    insert_sql = "insert into pythontest.pupper (name,sex,weight,height,color,date_of_birth,breed_id) values (%s,%s,%s,%s,%s,%s,%s)"
+    data = (pupper.name, pupper.sex, pupper.weight, pupper.height, pupper.color, pupper.date_of_birth, pupper.breed_id)
     dbConnector.writePupper(insert_sql, data)
 
