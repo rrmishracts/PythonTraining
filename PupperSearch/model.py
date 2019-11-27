53c1c83f8145ca3d817c43819f1f725a8e7f110b
Ranjan Mishra <58240696+rrmishracts@users.noreply.github.com>
2019-11-27 11:00:48 -0500

    Update model.py
    update model.py

--- PupperSearch/model.py
+++ PupperSearch/model.py
@@ -1,1 +1,40 @@
-test
+#!/usr/bin/python
+
+import mysql.connector
+
+class breed:
+   breedCount = 0
+
+   def __init__(self, id, name, temperament, coat):
+      self.id = id
+      self.name = name
+      self.temperament = temperament
+      self.coat = coat
+	  
+   def readbreed(self, id, name, temperament, coat):
+      print("")
+	  
+   def writebreed(self, id, name, temperament, coat):
+      print("")
+
+class pupper:
+   pupperCount = 0
+
+   def __init__(self, id, name, sex, weight, height, color, birthdate, breed):
+      self.id = id
+      self.name = name
+      self.sex = sex
+      self.weight = weight
+      self.height = height
+      self.color = color
+      self.birthdate = birthdate
+      self.breed = breed
+	  
+   def readpupper(self, id, name, sex, weight, height, color, birthdate, breed):
+      print("")
+	  
+   def writepupper(self, id, name, sex, weight, height, color, birthdate, breed):
+      print("")
+
+
+
